9a5a53a9b1c13353c4780831e69e3ea57eee2f9f
PiroHiroPiro <hirohiro11071107@gmail.com>
2019-01-15 02:04:01 +0900

    fix args error check

--- workspace/head/head.c
+++ workspace/head/head.c
@@ -5,7 +5,7 @@ static void do_head(FILE *f, long nlines);
 
 int main(int argc, char *argv[])
 {
-    if(argc != 2)
+    if(argc < 2)
     {
         fprintf(stderr, "Usage: %s\n", argv[0]);
         exit(1);
